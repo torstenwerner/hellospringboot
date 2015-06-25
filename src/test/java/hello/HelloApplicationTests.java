a88bd57c4bd67bc8f4d0b8f636a01809d89526e9
torstenwerner <mail.twerner@gmail.com>
2015-06-25 13:29:17 +0200

    @WebintegrationTest

--- src/test/java/hello/HelloApplicationTests.java
+++ src/test/java/hello/HelloApplicationTests.java
@@ -3,10 +3,12 @@ package hello;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = HelloApplication.class)
+@WebIntegrationTest
 public class HelloApplicationTests {
 
 	@Test
